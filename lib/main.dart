6a9583c8f360822f421249302634407d66c126b7
ywade <youwdhs@126.com>
2019-08-24 13:45:36 +0800

    路由管理

--- lib/main.dart
+++ lib/main.dart
@@ -1,116 +1,147 @@
-// Copyright 2018 The Flutter team. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-
 import 'package:flutter/material.dart';
-// import 'package:english_words/english_words.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
+    /// Material库中提供的Flutter APP框架，设置应用的名称、主题、语言、首页以及路由列表
+    return new MaterialApp(
+      title: "hi youwd",
+      theme: new ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      // home: new MyHomePage(title: "home page"),
+      routes: {
+        "pages_account_detail": (context) => PagesAccountDetail(text: ModalRoute.of(context).settings.arguments,),
+        "/": (context) => MyHomePage(title: 'Flutter Demo Home Page'), //注册首页路由
+      },
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   final String title;
 
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  _MyHomePageState createState() => new _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  int _counter = 1;
 
-  void _incrementCounter() {
+  /// 自增
+  void _incrementCount() {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
       _counter++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    /**Scaffold：页面脚手架，包含导航栏和body */
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text(widget.title),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+      body: new Center(
+        child: new Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
+            new Text("你已经点击了："),
+            new Text('$_counter', style: Theme.of(context).textTheme.display1),
+            FlatButton(
+              child: Text("打开我的页面"),
+              textColor: Colors.blue,
+              onPressed: () {
+                // 导航到我的页面
+                Navigator.push(context, MaterialPageRoute(
+                  builder: (context) {
+                    return PagesAccount();
+                  },
+                  // fullscreenDialog: true,
+                ));
+              },
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: '增加',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-      // floatingActionButtonLocation: FloatingActionButtonLocation.endDocked,//按钮的位置
+      floatingActionButton: new FloatingActionButton(
+        onPressed: _incrementCount,
+        child: new Icon(Icons.add),
+      ),
+    );
+  }
+}
+
+/// 子页面：我的账户
+class PagesAccount extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // String result  = "";
+    return Scaffold(
+      appBar: AppBar(title: Text("我的")),
+      body: Center(
+          child: Column(
+        children: <Widget>[
+          Text("我的账户页面haha1"),
+          // RaisedButton(
+          //   onPressed: () async {
+          //     var result = await Navigator.push(context,
+          //         MaterialPageRoute(builder: (context) {
+          //       return PagesAccountDetail(
+          //         text: "我是父页面传递过来的值",
+          //       );
+          //     }));
+          //     print("路由返回的值：$result");
+          //   },
+          //   child: Text("我的账户详情页面"),
+          // ),
+          RaisedButton(
+            onPressed: () async {
+             var  result = await Navigator.of(context)
+                  .pushNamed("pages_account_detail", arguments: "我是账户页面传递的值");
+              print("路由返回的值：$result");
+            },
+            child: Text("路由表形式打开页面"),
+          ),
+          // Text("$result"),
+        ],
+      )),
+    );
+  }
+}
+
+/// 路由传值  父子页面之间的传递
+class PagesAccountDetail extends StatelessWidget {
+  PagesAccountDetail({
+    Key key,
+    @required this.text, // 接收一个text参数
+  }) : super(key: key);
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    var args = ModalRoute.of(context).settings.arguments;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("账户详情页面"),
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(18),
+        child: Center(
+          child: Column(
+            children: <Widget>[
+              Text(args),
+              RaisedButton(
+                onPressed: () => Navigator.pop(context, "我是子页面返回的值"),
+                child: Text("返回"),
+              )
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
